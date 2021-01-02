dfbb143274b50372f82fc05eb7518ca2b0c8066b
Lance Wicks <lw@judocoach.com>
2021-01-02 12:01:14 +0000

    Perltidy applied

--- lib/vwjl.pm
+++ lib/vwjl.pm
@@ -19,7 +19,7 @@ post '/login' => sub {
     if ( _is_valid( params->{user}, params->{password} ) ) {
         warn "Logging in " . params->{user};
         session 'user' => params->{user};
-        
+
         if ( params->{user} eq 'lancew' ) {
             session 'admin' => true;
         }
@@ -42,60 +42,55 @@ get '/register' => sub {
 
 post '/register' => sub {
     my $phrase = passphrase( params->{'password'} )->generate;
-   
-    unless (
-        params->{user} && params->{password} && params->{password2}
-    ) {
+
+    unless ( params->{user} && params->{password} && params->{password2} ) {
         warn 'Missing Fields';
-        return template 'register' => { error => "All fields must be filled"};
+        return template 'register' =>
+            { error => "All fields must be filled" };
     }
 
-    unless (params->{password} eq params->{password2}) {
+    unless ( params->{password} eq params->{password2} ) {
         warn 'Passwords must match';
-        return template 'register' => { error => "Passwords must match"};
+        return template 'register' => { error => "Passwords must match" };
     }
 
-    if (_is_username_in_db( params->{user} )) {
+    if ( _is_username_in_db( params->{user} ) ) {
         warn 'Username already exists';
-        return template 'register' => { error => "This user name already exists in the system"};
+        return template 'register' =>
+            { error => "This user name already exists in the system" };
     }
-
 
     warn '----------------------+++++++++++', $phrase->rfc2307;
 
     my $dbh = DBI->connect( "dbi:Pg:dbname=postgres;host=localhost",
         'postgres', 'somePassword', { AutoCommit => 1 } );
-    
+
     $dbh->do( "
       INSERT INTO accounts
       (username,email,passphrase,created_on)
       VALUES
       ( ?, ?,? , localtimestamp)
-    ",undef, params->{user}, '-',$phrase->rfc2307 );
+    ", undef, params->{user}, '-', $phrase->rfc2307 );
 
     $dbh->disconnect;
 
     redirect '/login';
 };
 
-
-
 sub _is_valid {
     my ( $user, $password ) = @_;
 
     my $dbh = DBI->connect( "dbi:Pg:dbname=postgres;host=localhost",
         'postgres', 'somePassword', { AutoCommit => 1 } );
-    my $user_data = $dbh->selectrow_hashref( 'SELECT * FROM accounts WHERE username = ?',
+    my $user_data
+        = $dbh->selectrow_hashref(
+        'SELECT * FROM accounts WHERE username = ?',
         undef, $user );
     $dbh->disconnect;
 
-
-
     return false unless $user_data;
-    
 
-
-    if ( passphrase($password)->matches($user_data->{passphrase}) ) {
+    if ( passphrase($password)->matches( $user_data->{passphrase} ) ) {
         return true;
     }
     else {
@@ -108,15 +103,13 @@ sub _is_username_in_db {
 
     my $dbh = DBI->connect( "dbi:Pg:dbname=postgres;host=localhost",
         'postgres', 'somePassword', { AutoCommit => 1 } );
-    my $user_data = $dbh->selectrow_hashref( 'SELECT username FROM accounts WHERE username = ?',
+    my $user_data
+        = $dbh->selectrow_hashref(
+        'SELECT username FROM accounts WHERE username = ?',
         undef, $user );
     $dbh->disconnect;
 
     return false unless $user_data;
 }
-
-
-
-
 
 true;
