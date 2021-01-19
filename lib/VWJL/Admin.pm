4aaa03afa14b0e3ec7da96d104fca6638d70bc8f
Lance Wicks <lw@judocoach.com>
2021-01-19 21:05:26 +0000

    Move data migration out of controller

--- lib/VWJL/Admin.pm
+++ lib/VWJL/Admin.pm
@@ -4,7 +4,6 @@ use Moo;
 use VWJL::Infrastructure;
 
 sub get_migration_data {
-    my $self = shift;
     my $inf = VWJL::Infrastructure->new;
 
     my %data;
@@ -32,5 +31,45 @@ sub get_migration_data {
 
     return %data;
 }
+
+sub run_migrations {
+    my $inf = VWJL::Infrastructure->new;
+
+    my $db_migration_level = -1;
+    eval {
+        $db_migration_level
+            = $inf->dbh->selectrow_array(
+            'SELECT db_migration_level FROM system');
+    };
+
+    my @migration_files = $inf->get_migration_files;
+    my $dir             = "$FindBin::Bin/../db";
+
+    for my $file ( sort @migration_files ) {
+        my $migration;
+
+        $file =~ /(\d{3})/;
+        if ($1) {
+            $migration = $1;
+        }
+
+        if ( $db_migration_level < $migration ) {
+
+            my $filename = $dir . '/' . $file;
+            $/ = undef;
+            open( my $fh, '<:encoding(UTF-8)', $filename )
+                or die "Could not open file '$filename' $!";
+            my $sql = <$fh>;
+            close $fh;
+
+            $inf->dbh->do($sql);
+            $inf->dbh->do( 'UPDATE system SET db_migration_level = ?',
+                undef, $migration );
+        }
+
+    }
+}
+
+
 
 1;
