04043372c4d8eb5abee4ec1ce45eb4aabf148511
Lance Wicks <lw@judocoach.com>
2021-01-20 00:52:16 +0000

    Add missing use statement and sorting of lines

--- lib/vwjl_athlete.pm
+++ lib/vwjl_athlete.pm
@@ -1,21 +1,24 @@
 package vwjl_athlete;
-use Dancer2;
+
+use strict;
+use warnings;
+
 use Dancer2::Plugin::Auth::Tiny;
-
+use Dancer2;
 use VWJL::Athlete;
+use VWJL::Infrastructure;
 use VWJL::Waza;
-
-our $VERSION = '0.1';
 
 get '/' => sub {
     redirect '/' unless session('user');
+
     my $athlete_srv = VWJL::Athlete->new;
-    my $waza_module = VWJL::Waza->new;
     my $inf         = VWJL::Infrastructure->new;
+    my $waza_module = VWJL::Waza->new;
 
     my $athlete      = $athlete_srv->get( user => session('user') );
-    my $waza         = $waza_module->all_names;
     my $competitions = $inf->get_competitions;
+    my $waza         = $waza_module->all_names;
 
     my $comps;
     for my $comp (@$competitions) {
@@ -23,10 +26,10 @@ get '/' => sub {
     }
 
     template 'athlete/index' => {
-        'title'        => 'VWJL Athlete',
         'athlete'      => $athlete,
-        'waza'         => $waza,
         'competitions' => $comps,
+        'title'        => 'VWJL Athlete',
+        'waza'         => $waza,
     };
 };
 
